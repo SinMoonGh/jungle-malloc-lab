2c6bc45d793db19079fd79d96c13c75ffcaac096
SinMoonGh <tity0009@naver.com>
2025-04-29 11:34:09 +0900

    checkout 전 커밋#

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "3 team",
     /* First member's full name */
-    "Harry Bovik",
+    "Myeong Hoon",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "jungle@gmail.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
